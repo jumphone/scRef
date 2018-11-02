814feb06d0c02c20c2df072c4675f586b44d614c
Zhang, Feng <jumphone@163.com>
2018-11-02 14:48:57 -0400

    Add files via upload

---
+++ Unsupervised_methods/run_methods_in_SAFE.R
@@ -0,0 +1,81 @@
+#!/home/zhangfeng/bin/bin/Rscript
+#######################################################################################
+# Description: Run SAFE and all the methods compared in SAFE for single cell data
+# Date: 2018-10-22 14:40
+# douyaguang
+# package required: 
+#    SAFEclustering:  for SAFEclustering algorithm
+#    NMI:             calculate ANMI
+#    cidr:            calculate ARI
+#######################################################################################
+
+usage <- "Usage: 
+    program  <input_data_path>  <output_data_path>  <core_number>
+        input_data_path:     the input single cell data path
+
+        output_data_path:    the output cluster result file
+"
+args <- commandArgs(T)
+if (TRUE %in% (c("-h", "-H", "--help") %in% args))  {
+	message(usage)
+	quit(save = "default", status = 0, runLast = TRUE)
+}
+input_path <- args[1]
+output_path <- args[2]
+cores_number <- 10
+if (length(args) > 2) cores_number <- as.numeric(args[3])
+time_info_file <- paste(output_path, "time", sep = ".")
+cat("", file = time_info_file)
+
+# SAFEclustering can not be installed directly by github, many dependent R packages
+# must be installed via BioConductor or github manually, after all the dependent R
+# packages have been installed, the the SAFEclustering can be install by instruction 
+# from github.
+library("SAFEclustering")
+
+home <- "/home/disk/deepexp/"
+# default seed in individual_clustering toturial
+seed <- 123 
+methods <- c("SC3", "CIDR", "Seurat", "tSNE", "SAFE")
+
+## loading dataset
+single_cell_data <- read.table(input_path, row.names = 1, header = T, sep = "\t", check.names = F)
+
+## run SC3 CIDR t-SNE+k-means methods compared by SAFE, The methods exec order is
+## SC3 -> CIDR -> Seurat -> tSNE for all the methods assigned TRUE.
+
+time_start <- as.numeric(Sys.time())
+
+single_cell_cluster_results <- individual_clustering(inputTags = single_cell_data,
+	datatype = "count", mt_filter = FALSE, nGene_filter = FALSE, 
+	SC3 = TRUE, gene_filter = FALSE, 
+	CIDR = TRUE, nPC.cidr = NULL, 
+	Seurat = TRUE, nPC.seurat = NULL, resolution = 0.9, 
+	tSNE = TRUE, dimensions = 3, perplexity = 30, 
+	SEED = seed,
+	time_info_file = time_info_file)
+
+time_end <- as.numeric(Sys.time())
+time_used <- time_end - time_start
+cat("other_methods:\t", time_used, "\n", sep = "", file = time_info_file, append = T)
+
+# SAFE program part. Ensemble learning based on the clustering result of the above methods.
+# Graph-partition algorithm is applied while perform ensemble learning. The Graph-partition
+# exec has already downloaded and compiled in the path:
+# "../5CompareMethods/SAFEclustering/single_cell_clustering"
+program_dir <- paste(home, "5CompareMethods/SAFEclustering/single_cell_clustering", sep = "/")
+
+time_start <- as.numeric(Sys.time())
+
+single_cell_cluster_ensemble <- SAFE(cluster_results = single_cell_cluster_results, 
+	program.dir = program_dir, MCLA = TRUE, CSPA = TRUE, HGPA = TRUE, SEED = seed)
+
+time_end <- as.numeric(Sys.time())
+time_used <- time_end - time_start
+cat("SAFE:\t", time_used, "\n", sep = "", file = time_info_file, append = T)
+
+## combine all the results together
+comb_results <- rbind(single_cell_cluster_results, single_cell_cluster_ensemble$optimal_clustering)
+rownames(comb_results) <- methods
+colnames(comb_results) <- colnames(single_cell_data)
+write.table(file = output_path, t(comb_results), quote = F, sep = "\t")
---
+++ Unsupervised_methods/run_pagoda2.R
@@ -0,0 +1,82 @@
+#!/home/zhangfeng/bin/bin/Rscript
+#######################################################################################
+# Description: Run pagoda2 for single cell expression data
+# Date: 2018-10-24 22:20
+# douyaguang
+# package required: 
+#    pagoda2:         basic pagoda2 algorithm package
+#    NMI:             calculate ANMI
+#    cidr:            calculate ARI
+#    igraph:          for cluster methods used by pagoda2
+#######################################################################################
+
+usage <- "Usage: 
+    program  <input_data_path>  <output_data_path>  <core_number>
+        input_data_path:     the input single cell data path
+
+        output_data_path:    the output cluster result file prefix, 
+                             the output will be two files, 
+                             one is cluster result by infomap method that ends with \"_infomap\"
+                             another one is cluster result by multilevel method that ends with
+                             \"_multilevel\"
+
+        core_number:         running core numbers (default: 10)
+"
+args <- commandArgs(T)
+if (TRUE %in% (c("-h", "-H", "--help") %in% args))  {
+	message(usage)
+	quit(save = "default", status = 0, runLast = TRUE)
+}
+input_path <- args[1]
+output_path <- args[2]
+cores_number <- 10
+if (length(args) > 2) cores_number <- as.numeric(args[3])
+time_info_file <- paste(output_path, "time", sep = ".")
+cat("core_number:\t", cores_number, "\n", sep = "", file = time_info_file, append = F)
+
+seed <- 123
+set.seed(seed)
+
+library(pagoda2)
+library(Matrix)
+
+single_cell_data <- read.table(input_path, row.names = 1, header = T, sep = "\t", check.names = F)
+
+time_start <- as.numeric(Sys.time())
+
+counts <- gene.vs.molecule.cell.filter(single_cell_data, min.cell.size = 500)
+r <- Pagoda2$new(as.matrix(counts), log.scale = TRUE, n.cores = cores_number)
+r$adjustVariance(plot = F, gam.k = 10)
+r$calculatePcaReduction(nPcs = 50, n.odgenes = 3e3)
+r$makeKnnGraph(k = 40, type = 'PCA', center = T, distance = "cosine")
+
+time_end <- as.numeric(Sys.time())
+time_used <- time_end - time_start
+cat(time_used, sep = "\n", file = time_info_file, append = T)
+
+library(igraph)
+# infomap cluster method
+
+time_start <- as.numeric(Sys.time())
+
+r$getKnnClusters(method = infomap.community, type = "PCA")
+infomap_result <- r$clusters$PCA$community
+
+time_end <- as.numeric(Sys.time())
+time_used <- time_end - time_start
+cat(time_used, sep = "\n", file = time_info_file, append = T)
+
+
+# multilevel cluster method
+time_start <- as.numeric(Sys.time())
+
+r$getKnnClusters(method = multilevel.community, type = "PCA")
+multilevel_result <- r$clusters$PCA$community
+
+time_end <- as.numeric(Sys.time())
+time_used <- time_end - time_start
+cat(time_used, sep = "\n", file = time_info_file, append = T)
+
+comb_results <- rbind(infomap_result, multilevel_result)
+rownames(comb_results) <- c("pogoda2_infomap", "pogoda2_multilevel")
+write.table(file = output_path, t(comb_results), quote = F, sep = "\t")
