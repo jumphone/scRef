ae71cb5ef2cd2d90ea7776aa1c8994fdd3a81df1
Zhang, Feng <jumphone@163.com>
2018-11-04 16:30:39 -0500

    Add files via upload

--- NET/addDegree.py
+++ NET/addDegree.py
@@ -2,17 +2,22 @@ import sys
 fi=open(sys.argv[1])
 fa=open(sys.argv[2])
 fo=open(sys.argv[1]+'.net','w')
-fa.readline()
+#fa.readline()
 DE={}
 MAX=1
 for line in fa:
     seq=line.rstrip().split('\t')
-    DE[seq[0]]=float(seq[1])
-    MAX=max(MAX,float(seq[1]))
+    seq[0]=seq[0].upper()
+    try:
+        DE[seq[0]]=float(seq[1])
+        MAX=max(MAX,float(seq[1]))
+    except Exception as e:
+        pass
 
 fo.write(fi.readline())
 for line in fi:
     seq=line.rstrip().split('\t')
+    seq[0]=seq[0].upper()
     print(seq[0])
     if seq[0] in DE:
          this_de= DE[seq[0]] 
