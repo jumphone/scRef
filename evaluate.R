dd0724bbac90eb406f99a531f8a07d7174a8c279
Zhang, Feng <jumphone@163.com>
2018-11-05 11:03:34 -0500

    Update evaluate.R

--- evaluate.R
+++ evaluate.R
@@ -1,12 +1,9 @@
 library('mclust')
 library('NMI')
 
-#TAG=read.table('TAG.txt',header=T,sep='\t')
 a=read.table('Kendall.txt',header=T,sep='\t')
-b=read.table('../../Tasic_exp_sc_mat_cluster_original.txt',header=T,sep='\t')
-c=read.table('../../Tasic_exp_sc_mat_cluster_merged.txt',sep='\t',header=T)
-
-
+b=read.table('../../Zeisel_exp_sc_mat_cluster_original.txt',header=T,sep='\t')
+c=read.table('../../Zeisel_exp_sc_mat_cluster_merged.txt',sep='\t',header=T)
 
 a[,2]=as.character(a[,2])
 a[which(a[,2]=='Myelinating.Oligodendrocytes'),2]='Oligodendrocytes'
@@ -24,10 +21,10 @@ real=data.frame(ids=ids,label=b[,2])
 print('NMI')
 NMI(real,pred)$value
 
-#########Merged######
+#########Neuron Merged######
 print('merge')
 print('ARI')
-r=which(!c[,2] %in% c('Unclassified'))
+r=which(!c[,2] %in% c('OTHER'))
 adjustedRandIndex(a[r,2],c[r,2])
 pred=data.frame(ids=ids[r],label=a[r,2])
 real=data.frame(ids=ids[r],label=c[r,2])
@@ -35,10 +32,10 @@ print('NMI')
 NMI(real,pred)$value
 
 
-#########Removed#####
+#########Neuron Removed#####
 print('remove')
 print('ARI')
-r=which(!c[,2] %in% c('Neuron','Unclassified'))
+r=which(!c[,2] %in% c('neurons'))
 adjustedRandIndex(a[r,2],b[r,2])
 pred=data.frame(ids=ids[r],label=a[r,2])
 real=data.frame(ids=ids[r],label=b[r,2])
