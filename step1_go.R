cf377ec7a9533a3f1f74649d30cdf5a06c0f68fd
Zhang, Feng <jumphone@163.com>
2018-11-07 12:14:22 -0500

    Update step1_go.R

--- step1_go.R
+++ step1_go.R
@@ -1,10 +1,23 @@
 source('scRef.R')
+
+
 exp_sc_mat=read.table('Zeisel_exp_sc_mat.txt',header=T,row.names=1,'\t')
 exp_ref_mat=read.table('Reference_expression.txt',header=T,row.names=1,sep='\t')
 
+#KENDALL
 out=.get_cor(exp_sc_mat, exp_ref_mat, method='kendall',CPU=4, print_step=10)
 tag=.get_tag_max(out)
-cat('finished!!!')
+cat('kendall finished!!!')
 write.table(tag,file='TAG.txt',quote=F,row.names=F,col.names=T,sep='\t')
 
 
+
+#SEMI-SUPERVISED
+out=.get_cor(exp_sc_mat, exp_ref_mat, method='kendall',CPU=4, print_step=10)
+tag=.get_tag_max(out)
+LocalRef=.generate_ref <- function(exp_sc_mat, tag)
+out=.get_log_p_sc_given_ref(exp_sc_mat, LocalRef, CPU=4, print_step=10)
+tag=.get_tag_max(out)
+cat('semi finished!!!')
+write.table(tag,file='TAG_SEMI.txt',quote=F,row.names=F,col.names=T,sep='\t')
+  
