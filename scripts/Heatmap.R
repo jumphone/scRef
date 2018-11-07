c8e6d3cff78ea9404148386402202e520eb3d666
Zhang, Feng <jumphone@163.com>
2018-11-06 21:09:12 -0500

    Update Heatmap.R

--- scripts/Heatmap.R
+++ scripts/Heatmap.R
@@ -2,5 +2,6 @@
 a=read.table('rank.txt',sep='\t',header=T,row.names=1)
 a=as.matrix(a)
 library('gplots')
-
+pdf('HEAT.pdf','w')
 heatmap.2(a,Rowv=F,Colv=F, dendrogram='none',col=colorpanel(256, low="Blue", high="Red", mid="grey90"), scale="row", key=TRUE, keysize=1, symkey=FALSE, densadj = 0.1, density.info="none", trace="none",margin=c(20,20),cexRow=2,cexCol=2)
+dev.off()
