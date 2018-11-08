fbfa11f9f61c841c46d81ad61ed1d8b6e3d90a28
Zhang, Feng <jumphone@163.com>
2018-11-08 16:11:27 -0500

    Update go.R

--- scripts/ALLEN/go.R
+++ scripts/ALLEN/go.R
@@ -4,10 +4,9 @@ meta_data=read.table('meta.txt',sep='\t',header=T)
 
 library(pcaPP)
 
-
+################################################################
 Stage=meta_data[,9]
 OUT=c()
-
 i=1
 while(i<=length(exp_data[,1])){
 this_gene=rownames(exp_data)[i]
@@ -17,19 +16,16 @@ OUT=cbind(OUT,c(this_gene,this_cor))
 i=i+1
 print(i)
 }
-
 OUT=t(OUT)
-
 OUT[which(OUT[,2]=='NaN'),2]='0'
 hist(as.numeric(OUT[,2]),breaks=100)
 save(OUT,file='COR.RData')
-
+################################################################
 COR=as.numeric(OUT[,2])
 STEM = which(COR < -0.5)
 STEM_GENES=OUT[STEM,1]
-
 save(STEM_GENES,file='STEM.RData')
-
+################################################################
 
 
 
