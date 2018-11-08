43d04f4ff2c007f453037560cafd10e220995341
Zhang, Feng <jumphone@163.com>
2018-11-08 15:58:31 -0500

    Update go.R

--- scripts/ALLEN/go.R
+++ scripts/ALLEN/go.R
@@ -2,3 +2,19 @@
 exp_data=read.table('exp_mat.txt',sep='\t',row.names=1,header=F)
 meta_data=read.table('meta.txt',sep='\t',header=T)
 
+library(pcaPP)
+
+
+Stage=meta_data[,9]
+OUT=c()
+
+i=1
+while(i<=length(exp_data[,1])){
+this_gene=rownames(exp_data)[i]
+this_exp=t(exp_data[i,])
+this_cor=cor.fk(this_exp, Stage)
+OUT=cbind(OUT,c(this_gene,this_cor))
+i=i+1
+}
+
+OUT=t(OUT)
