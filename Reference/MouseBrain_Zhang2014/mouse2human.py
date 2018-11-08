1af297b6daa03fb9b07d54484118ca787a5c7a1c
Zhang, Feng <jumphone@163.com>
2018-11-08 17:14:10 -0500

    Add files via upload

---
+++ Reference/MouseBrain_Zhang2014/mouse2human.py
@@ -0,0 +1,34 @@
+import sys
+HGNC2human={}
+mouse2HGNC={}
+
+fa=open('./hgnc_complete_set.txt')
+for line in fa:
+    if "protein-coding" in line:
+
+        seq=line.split('\t')
+        hgnc=seq[0]
+        symbol=seq[1]
+        HGNC2human[hgnc]=symbol
+
+
+
+fa=open('./MGI_HGNC_homologene.rpt')
+for line in fa:
+    seq=line.rstrip().split('\t')
+    sym=seq[1]
+    ens=seq[9]
+    hgnc=seq[20]
+    mouse2HGNC[sym]=hgnc
+    
+
+fi=open(sys.argv[1])
+fo=open(sys.argv[1]+'.human','w')
+fo.write(fi.readline())
+for line in fi:
+    seq=line.rstrip().split('\t')
+    mouse=seq[0]
+    if mouse in mouse2HGNC and mouse2HGNC[mouse] in HGNC2human:
+        hgnc=mouse2HGNC[mouse]
+        human=HGNC2human[hgnc]
+        fo.write(human+'\t'+'\t'.join(seq[1:])+'\n')
