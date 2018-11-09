699c8412453b381cc37ce85eda25516d6a7063b3
Zhang, Feng <jumphone@163.com>
2018-11-09 11:50:30 -0500

    Update scRef.R

--- scRef.R
+++ scRef.R
@@ -151,6 +151,7 @@
     if(refnames==FALSE){
         refnames=names(table(TAG[,2]))}
         else{refnames=refnames}
+    outnames=c()
     for(one in refnames){
         this_col=which(TAG[,2]==one)
         if(length(this_col)>= min_cell){
