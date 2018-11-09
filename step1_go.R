8199c0eb7db07787fe892076b7851e0c5c78bc6d
Zhang, Feng <jumphone@163.com>
2018-11-08 22:18:51 -0500

    Update step1_go.R

--- step1_go.R
+++ step1_go.R
@@ -12,7 +12,7 @@ write.table(tag,file='TAG.txt',quote=F,row.names=F,col.names=T,sep='\t')
 #SEMI-SUPERVISED
 out=.get_cor(exp_sc_mat, exp_ref_mat, method='kendall',CPU=4, print_step=10)
 tag=.get_tag_max(out)
-LocalRef=.generate_ref(exp_sc_mat, tag)
+LocalRef=.generate_ref(exp_sc_mat, tag, min_cell=1)
 out=.get_log_p_sc_given_ref(exp_sc_mat, LocalRef, CPU=4, print_step=10)
 tag=.get_tag_max(out)
 cat('semi finished!!!')
