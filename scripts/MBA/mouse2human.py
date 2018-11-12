61b45a0a85ab3311da248225f2b6df7986b08d7b
Zhang, Feng <jumphone@163.com>
2018-11-12 15:11:32 -0500

    Add files via upload

---
+++ scripts/MBA/mouse2human.py
@@ -0,0 +1,37 @@
+import sys
+HGNC2human={}
+mouse2HGNC={}
+
+fa=open('./hgnc_complete_set.txt')
+for line in fa:
+    if "protein-coding" in line:
+
+        seq=line.split('\t')
+        hgnc=seq[0]
+        symbol=seq[1]
+        HGNC2human[hgnc]=symbol
+
+
+
+fa=open('./MGI_HGNC_homologene.rpt')
+for line in fa:
+    seq=line.rstrip().split('\t')
+    sym=seq[1]
+    ens=seq[9]
+    hgnc=seq[20]
+    mouse2HGNC[sym]=hgnc
+    
+
+fi=open(sys.argv[1])
+fo=open(sys.argv[2],'w')
+fo.write(fi.readline())
+old=set()
+for line in fi:
+    seq=line.rstrip().split('\t')
+    mouse=seq[0]
+    if mouse in mouse2HGNC and mouse2HGNC[mouse] in HGNC2human:
+        hgnc=mouse2HGNC[mouse]
+        human=HGNC2human[hgnc]
+        if human not in old:
+            old.add(human)
+            fo.write(human+'\t'+'\t'.join(seq[1:])+'\n')
