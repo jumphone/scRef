ffa7d58c127b6eef3584d1abaf4d5e9be3bcbe6d
Zhang, Feng <jumphone@163.com>
2018-11-14 13:44:23 -0500

    Update prepare.R

--- scripts/Embed/prepare.R
+++ scripts/Embed/prepare.R
@@ -1,1 +1,12 @@
 
+library(Seurat)
+library(dplyr)
+pbmc.data <- read.table('CASE.txt',sep='\t',check.name=F,row.names=1,header=T)
+save(pbmc,file='CASE_raw.RObj')
+pbmc <- CreateSeuratObject(raw.data = pbmc.data, min.cells = 3, min.genes = 200, project = "Natalie")
+
+mito.genes <- grep(pattern = "^mt-", x = rownames(x = pbmc@data), value = TRUE)
+percent.mito <- Matrix::colSums(pbmc@raw.data[mito.genes, ])/Matrix::colSums(pbmc@raw.data)
+pbmc <- AddMetaData(object = pbmc, metadata = percent.mito, col.name = "percent.mito")
+pbmc <- AddMetaData(object = pbmc, metadata = pbmc@ident, col.name = "batch")
+pbmc=FilterCells(object = pbmc, subset.names = c("nGene", "percent.mito"), low.thresholds = c(500, -Inf), high.thresholds = c(3000, 0.1))
