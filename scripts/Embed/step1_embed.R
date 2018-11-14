3922a9a4a85d4dc1d04c61acb8d65a199f34aca9
Zhang, Feng <jumphone@163.com>
2018-11-14 14:41:52 -0500

    Update step1_embed.R

--- scripts/Embed/step1_embed.R
+++ scripts/Embed/step1_embed.R
@@ -21,6 +21,14 @@ exp_sc_mat=read.table('WT.txt',sep='\t',check.name=F,row.names=1,header=T)
 out=.get_cor(exp_sc_mat, LocalRef, method='kendall',CPU=10, print_step=10)
 tag=.get_tag_max(out)
 
+W=(1-out)/2
+
+getw=function(X){
+  X=X/sum(X)
+  return(X)
+}
+
+W=apply(W,2,getw)
 
 
 
