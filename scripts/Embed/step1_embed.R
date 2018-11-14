ac4b1fbf3960f528ef9fa9b06fc1b12176734dc8
Zhang, Feng <jumphone@163.com>
2018-11-14 14:45:48 -0500

    Update step1_embed.R

--- scripts/Embed/step1_embed.R
+++ scripts/Embed/step1_embed.R
@@ -3,6 +3,10 @@ library(Seurat)
 source('scRef.R')
 load('CASE.RObj')
 
+
+TSNE_VEC=pbmc@dr$tsne@cell.embeddings
+IDENT=pbmc@ident
+
 COL=c()
 i=1
 while(i <=length(pbmc@ident)){
@@ -21,14 +25,8 @@ exp_sc_mat=read.table('WT.txt',sep='\t',check.name=F,row.names=1,header=T)
 out=.get_cor(exp_sc_mat, LocalRef, method='kendall',CPU=10, print_step=10)
 tag=.get_tag_max(out)
 
-W=(1-out)/2
 
-getw=function(X){
-  X=X/sum(X)
-  return(X)
-}
 
-W=apply(W,2,getw)
 
 
 
