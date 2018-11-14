8f6e4c2fe061c12f80cbf98f319bd402dfbd33b2
Zhang, Feng <jumphone@163.com>
2018-11-14 14:37:22 -0500

    Update step1_embed.R

--- scripts/Embed/step1_embed.R
+++ scripts/Embed/step1_embed.R
@@ -16,5 +16,11 @@ exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
 LocalRef=.generate_ref(exp_sc_mat, tag, min_cell=10)
 
 
+exp_sc_mat=read.table('WT.txt',sep='\t',check.name=F,row.names=1,header=T)
+
+out=.get_cor(exp_sc_mat, LocalRef, method='kendall',CPU=10, print_step=10)
+tag=.get_tag_max(out)
+
+
 
 
