e6f35681c62610ade4cbf42f78a9527bf10f43fb
Zhang, Feng <jumphone@163.com>
2018-11-14 14:34:21 -0500

    Update step1_embed.R

--- scripts/Embed/step1_embed.R
+++ scripts/Embed/step1_embed.R
@@ -2,3 +2,19 @@
 library(Seurat)
 source('scRef.R')
 load('CASE.RObj')
+
+COL=c()
+i=1
+while(i <=length(pbmc@ident)){
+    this_col=which(colnames(pbmc@raw.data)==names(pbmc@ident)[i])
+    COL=c(COL,this_col)
+    i=i+1
+    } 
+
+tag=cbind(names(pbmc@ident),pbmc@ident)
+exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
+LocalRef=.generate_ref(exp_sc_mat, tag, min_cell=10)
+
+
+
+
