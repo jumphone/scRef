01882df40014489a2841dc5885010a73f1c937e4
Zhang, Feng <jumphone@163.com>
2018-11-14 14:57:32 -0500

    Update step1_embed.R

--- scripts/Embed/step1_embed.R
+++ scripts/Embed/step1_embed.R
@@ -5,7 +5,9 @@ load('CASE.RObj')
 
 
 TSNE_VEC=pbmc@dr$tsne@cell.embeddings
-IDENT=pbmc@ident
+IDENT=as.character(pbmc@ident)
+
+
 
 COL=c()
 i=1
@@ -16,6 +18,7 @@ while(i <=length(pbmc@ident)){
     } 
 
 tag=cbind(names(pbmc@ident),pbmc@ident)
+exp_ref_mat=as.matrix(pbmc@raw.data)[,COL]
 exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
 LocalRef=.generate_ref(exp_sc_mat, tag, min_cell=10)
 
@@ -28,5 +31,21 @@ tag=.get_tag_max(out)
 
 
 
+i=1
+while(i<=length(tag[,1])){
+    this_tag = as.character(tag[i,2])
+    
+    vec_index=which(IDENT==this_tag)
+    
+    
+    exp_ref_mat[,vec_index]
+    
+    i=i+1}
 
+
+library("RColorBrewer")
+display.brewer.all()
+brewer.pal(n=8,name='Set2')
+#install.packages("wesanderson")
+library(wesanderson)
 
