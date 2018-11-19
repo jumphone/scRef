8d0f1d58b1f4db00df2c704226ea31ca794f703a
Zhang, Feng <jumphone@163.com>
2018-11-19 13:34:03 -0500

    Update SimulatedData_go.R

--- scripts/TsneProjection/SimulatedData_go.R
+++ scripts/TsneProjection/SimulatedData_go.R
@@ -1,5 +1,9 @@
 load('TSNE.RData')
 library('Seurat')
 source('scRef.R')
+ori_label=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,sep='\t')
+pbmc@meta.data$ori=ori_label[,2]
+
+TSNEPlot(pbmc, group.by="ori")
 
 
