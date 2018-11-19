fc94658b6733608710cf30217fc61810ffc8c029
Zhang, Feng <jumphone@163.com>
2018-11-19 13:41:33 -0500

    Update SimulatedData_go.R

--- scripts/TsneProjection/SimulatedData_go.R
+++ scripts/TsneProjection/SimulatedData_go.R
@@ -6,4 +6,16 @@ pbmc@meta.data$ori=ori_label[,2]
 
 TSNEPlot(pbmc, group.by="ori")
 
+table(pbmc@meta.data$ori)
+ref_vec=pbmc@dr$tsne@cell.embeddings
+
+#COL=rep('gray80',length(ref_vec[,1]))
+
+
+OLIG=which(pbmc@meta.data$ori=='oligodendrocytes')
+
+plot(ref_vec[,1],ref_vec[,2], pch=16, col='gray80', xlim=c(-30, 30), ylim=c(-40,35), xlab='tSNE_1',ylab='tSNE_2')
+par(new=T)
+plot(ref_vec[OLIG,1],ref_vec[OLIG,2], pch=16, col='red', xlim=c(-30, 30), ylim=c(-40,35), xlab='',ylab='')
+
 
