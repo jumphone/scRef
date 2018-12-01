23e622326c194877b598df93fc94d9f551bc4332
Zhang, Feng <jumphone@163.com>
2018-12-01 18:25:51 -0500

    Update scRef.R

--- scRef.R
+++ scRef.R
@@ -357,7 +357,7 @@ SCREF <- function(exp_sc_mat, exp_ref_mat, method1='kendall', method2='multinomi
 
 
 
-.trajectory = function(sim_mat, plot_type='polygon', random_ratio=0.03, random_seed=123, do.label=TRUE, label_dist=1.2, label_size=3,cell_size=1,plot_size=1.5){
+.trajectory = function(sim_mat, plot_type='none', random_ratio=0.03, random_seed=123, do.label=TRUE, label_dist=1.2, label_size=3,cell_size=1,plot_size=1.5){
 
     library(MASS)
     library(ggplot2)
@@ -435,11 +435,6 @@ SCREF <- function(exp_sc_mat, exp_ref_mat, method1='kendall', method2='multinomi
         p=p+stat_density2d(aes(alpha=..density..), geom="tile", contour=FALSE)
     }else{p=p}
 
-    if(do.label==TRUE){
-        target_df=data.frame(VEC* label_dist); colnames(target_df) = c("x","y")
-        p=p+geom_text(data=target_df,aes(label=rownames(target_df)), colour='black',size=label_size)
-        }
-
     seg_vec=c()
     i=1
     while(i<=length(VEC[,1])-1){
@@ -458,6 +453,11 @@ SCREF <- function(exp_sc_mat, exp_ref_mat, method1='kendall', method2='multinomi
         p <- p + geom_segment(x=seg_vec[i,1], y=seg_vec[i,2], xend=seg_vec[i,3], yend=seg_vec[i,4],color="red", linetype="dashed")
         p <- p + geom_point(x=(seg_vec[i,1]+seg_vec[i,3])/2, y=(seg_vec[i,2]+seg_vec[i,4])/2, colour='red', ,size=1.5)
     }
+
+    if(do.label==TRUE){
+        target_df=data.frame(VEC* label_dist); colnames(target_df) = c("x","y")
+        p=p+geom_text(data=target_df,aes(label=rownames(target_df)), colour='black',size=label_size)
+        }
 
     output=list()
     output$ggplot = p
