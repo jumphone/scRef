726f0b03d40b89068267f67e9934b2a73e2a1e89
Zhang, Feng <jumphone@163.com>
2018-12-02 09:26:26 -0500

    Update getPatient.py

--- scripts/Trajectory/getPatient.py
+++ scripts/Trajectory/getPatient.py
@@ -1,23 +1,23 @@
 import sys
 fi=open('GSE70630_OG_processed_data_v2.txt')
-#fo=open('GSE70630_OG_processed_data_v2_MGH54.txt','w')
+fo=open('GSE70630_OG_processed_data_v2_MGH54.txt','w')
 #fo=open('GSE70630_OG_processed_data_v2_MGH36.txt','w')
 #fo=open('GSE70630_OG_processed_data_v2_MGH53.txt','w')
 #fo=open('GSE70630_OG_processed_data_v2_MGH60.txt','w')
 #fo=open('GSE70630_OG_processed_data_v2_MGH93.txt','w')
-fo=open('GSE70630_OG_processed_data_v2_MGH97.txt','w')
+#fo=open('GSE70630_OG_processed_data_v2_MGH97.txt','w')
 header=fi.readline().rstrip().split('\t')
 
 tmp=[]
 i=0
 while i<len(header):
     #print(header[i])
-    #if 'MGH54' in header[i] or header[i][:2]=='54':
+    if 'MGH54' in header[i] or header[i][:2]=='54':
     #if 'MGH36' in header[i] or header[i][:2]=='36':
     #if 'MGH53' in header[i] or header[i][:2]=='53':
     #if 'MGH60' in header[i] or header[i][:2]=='60':
     #if 'MGH93' in header[i] or header[i][:2]=='93':
-    if 'MGH97' in header[i] or header[i][:2]=='97':
+    #if 'MGH97' in header[i] or header[i][:2]=='97':
         tmp.append(i)
     i=i+1
 
@@ -39,7 +39,8 @@ for line in fi:
             this_exp=str(float(seq[one]))
         except Exception as e:
             this_exp='0.0'
-
+        if this_exp=='nan':
+            this_exp='0.0'
         fo.write('\t'+this_exp)
     fo.write('\n')
 
