c1f7283d5b67adfb224990bfd593b20df131a9db
Zhang, Feng <jumphone@163.com>
2018-12-01 23:06:04 -0500

    Create run.R

---
+++ scripts/Trajectory/run.R
@@ -0,0 +1,32 @@
+
+library('Seurat')
+source('scRef.R')
+
+exp_raw_data= read.table('GSE70630_OG_processed_data_v2_MGH54.txt',sep='\t',header=T,row.names=1)
+pbmc <- CreateSeuratObject(raw.data =exp_raw_data, min.cells = 3, min.genes = 200, project = "10X_PBMC")
+
+exp_ref_mat=read.table('Reference_expression_human.txt',header=T,row.names=1,sep='\t',check.name=F)
+a=SCREF(exp_sc_mat, exp_ref_mat)
+out=a$out2
+tag=a$tag2
+
+rownames(out)[which(rownames(out)=='Newly Formed Oligodendrocyte')]='Oligodendrocytes'
+result=.trajectory(out, plot_type='polygon', plot_size=1.7, cell_size=2,label_dist=1.2, label_size=10, random_ratio=0.03)
+png(filename = "TraOligGlioma.png",width = 1024, height = 1024)
+result$ggplot
+dev.off()
+
+
+
+
+pbmc=readRDS('GSE75330.RDS')
+exp_ref_mat=read.table('GSE75330_mouse_combined_reference.txt',header=T,row.names=1,sep='\t')
+
+# Reference-based annotation - multinomial
+out=.get_log_p_sc_given_ref(pbmc@raw.data, exp_ref_mat)
+
+# Construct trajectory
+result=.trajectory(out, plot_type='polygon', plot_size=1.7, cell_size=2,label_dist=1.2, label_size=10, random_ratio=0.03)
+png(filename = "TraOPC.png",width = 1024, height = 1024)
+result$ggplot
+dev.off()
