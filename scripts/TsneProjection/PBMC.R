84e6add36ccf259a98b319bc4a5dac9997e1358f
Zhang, Feng <jumphone@163.com>
2018-12-05 14:06:18 -0500

    Update PBMC.R

--- scripts/TsneProjection/PBMC.R
+++ scripts/TsneProjection/PBMC.R
@@ -1,6 +1,37 @@
 load('pbmc.RData')
 source('scRef.R')
+
 library(Seurat)
+
+
+COL=c()
+i=1
+while(i <=length(pbmc@ident)){
+    this_col=which(colnames(pbmc@raw.data)==names(pbmc@ident)[i])
+    COL=c(COL,this_col)
+    i=i+1
+    } 
+
+#ref_tag=cbind(names(pbmc@ident),as.character(pbmc@ident))    
+exp_raw_mat=as.matrix(pbmc@raw.data)[,COL]
+exp_sc_mat=exp_raw_mat[,which(ref_tag[,2]=='5')] # NK cells
+
+
+
+
+
+raw_mat=read.table('PeripheralBlood_com.txt',header=T,row.names=1,sep='\t')
+pbmc <- CreateSeuratObject(raw.data = raw_mat, min.cells = 3, min.genes = 200, 
+    project = "10X_PBMC")
+
+pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", 
+    scale.factor = 10000)
+pbmc <- FindVariableGenes(object = pbmc, do.plot=F,mean.function = ExpMean, dispersion.function = LogVMR, 
+    x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
+pbmc <- ScaleData(object = pbmc, vars.to.regress = c("nUMI"))
+
+
+
 
 
 
