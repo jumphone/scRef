fd717f2b0a84e9bdfc17bde8eff901e6d705ce1f
Zhang, Feng <jumphone@163.com>
2018-12-07 00:55:30 -0500

    Update pure.py

--- scripts/HUMAN/GSE72056/pure.py
+++ scripts/HUMAN/GSE72056/pure.py
@@ -13,7 +13,7 @@ fo.write(fi.readline().replace('1','T-cells').replace('2','B-cells').replace('3'
 fo=open('GSE72056_melanoma_single_cell_revised_v2.txt.pure','w')
 old=set()
 for line in fi:
-    seq=line.rstrip('\t')
+    seq=line.rstrip().split('\t')
     if seq[0] not in old:
     	fo.write(line)
     	old.add(seq[0])
