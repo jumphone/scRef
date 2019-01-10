3b4bc75fbc577f63da580204a28e2e69b7411857
Zhang, Feng <jumphone@163.com>
2019-01-10 14:51:55 -0500

    Update go_intestine_TDE.R

--- scripts/Trajectory/go_intestine_TDE.R
+++ scripts/Trajectory/go_intestine_TDE.R
@@ -4,6 +4,18 @@ pbmc <- CreateSeuratObject(raw.data = exp_mat, min.cells = 0, min.genes = 0, pro
 tag=cbind(as.character(pbmc@meta.data$orig.ident),as.character(pbmc@meta.data$orig.ident))
 source('scRef.R')
 NewRef=.generate_ref(exp_mat,tag)
+out=.get_log_p_sc_given_ref(pbmc@raw.data, NewRef)
+result=.trajectory(out, plot_type='polygon', plot_size=1.7, label_dist=1.2, label_size=10, random_ratio=0.03)
 
+png(filename = "TDE.png",width = 1024, height = 1024)
+result$ggplot
+dev.off()
 
+library(igraph)
 
+MST=.generate_mst(result$mat)
+pdf('MST.pdf')
+plot(MST)
+dev.off()
+
+save.image('TDE_intestine.RData')
