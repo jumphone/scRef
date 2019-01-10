4f4a3789c76f8b8a66e907045d80c0f0f28b1e56
Zhang, Feng <jumphone@163.com>
2019-01-10 15:01:58 -0500

    Update go_intestine_TDE.R

--- scripts/Trajectory/go_intestine_TDE.R
+++ scripts/Trajectory/go_intestine_TDE.R
@@ -19,3 +19,29 @@ plot(MST)
 dev.off()
 
 save.image('TDE_intestine.RData')
+
+###########TSCAN##########################
+
+library(TSCAN)
+COL=c()
+i=1
+while(i <=length(pbmc@ident)){
+    this_col=which(colnames(pbmc@raw.data)==names(pbmc@ident)[i])
+    COL=c(COL,this_col)
+    i=i+1
+    } 
+exp_sc_mat=as.matrix(pbmc@raw.data)[,COL]
+VAR_GENE=apply(exp_sc_mat,1,var)
+USED_GENE=which(VAR_GENE>0)
+used_data=exp_sc_mat[USED_GENE,]
+
+
+procdata <- preprocess(used_data,clusternum=15, pseudocount = 1, minexpr_value = 1, minexpr_percent = 0.4, cvcutoff = 0.5)
+lpsmclust <- exprmclust(procdata, clusternum=15 )
+tmp=names(lpsmclust$clusterid)
+lpsmclust$clusterid=pbmc@meta.data$orig.ident
+names(lpsmclust$clusterid)=tmp
+
+pdf('TSCAN_InstestineDEV.pdf',width=14,height=14)
+plotmclust(lpsmclust,show_cell_names=F,cell_name_size =1)
+dev.off()
