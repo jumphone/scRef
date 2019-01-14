f61635b1444a1dc8136f8a0e7b9941392d74c729
Zhang, Feng <jumphone@163.com>
2019-01-14 00:36:00 -0500

    Update sup.R

--- scripts/TsneProjection/sup.R
+++ scripts/TsneProjection/sup.R
@@ -95,16 +95,80 @@ plot(
   )
 )
 
+table(scmapCell_clusters$scmap_cluster_labs)
+
 ####################################
 ####################################
 ####################################
 #Human NK cell
 
+load('tsnealign_pbmc.RObj')
+library(Seurat)
+library(SingleCellExperiment)
+library(scmap)
+#source('scRef.R')
+
+tmp=as.matrix(as.factor(pbmc@meta.data$orig.ident))
+#tmp[,1]=as.factor(tmp[,1])
+rownames(tmp)=colnames(pbmc@data)
+colnames(tmp)="cell_type1"
+ann=tmp
+
+
+set.seed(1)
+yan=as.matrix(pbmc@data)
+sce <- SingleCellExperiment(assays = list(normcounts = as.matrix(yan)), colData = ann)
+rowData(sce)$feature_symbol <- rownames(sce)
+logcounts(sce) <- normcounts(sce)
+sce <- selectFeatures(sce, suppress_plot = FALSE)
+
+set.seed(1)
+sce <- indexCell(sce)
+names(metadata(sce)$scmap_cell_index)
+length(metadata(sce)$scmap_cell_index$subcentroids)
+dim(metadata(sce)$scmap_cell_index$subcentroids[[1]])
+metadata(sce)$scmap_cell_index$subcentroids[[1]][,1:5]
+
+set.seed(123)
+sim_exp_sc_mat=exp_sc_mat
+sim_ann=as.matrix(rep('NK.cell',ncol(sim_exp_sc_mat)))
+rownames(sim_ann)=colnames(sim_exp_sc_mat)
+colnames(sim_ann)='cell_type1'
+
+ssce=SingleCellExperiment(assays = list(normcounts = as.matrix(sim_exp_sc_mat)), colData = sim_ann)
+rowData(ssce)$feature_symbol <- rownames(ssce)
+logcounts(ssce) <- normcounts(ssce)
+ssce <- selectFeatures(ssce, suppress_plot = FALSE)
 
 
 
+scmapCell_results <- scmapCell(
+  ssce, 
+  list(
+    yan = metadata(sce)$scmap_cell_index
+  )
+)
 
 
+scmapCell_clusters <- scmapCell2Cluster(
+  scmapCell_results, threshold = 0,w=1,
+  list(
+    as.character(colData(sce)$cell_type1)
+  )
+)
 
+head(scmapCell_clusters$combined_labs)
+
+
+plot(
+  getSankey(
+    colData(ssce)$cell_type1, 
+    scmapCell_clusters$scmap_cluster_labs[,"yan"],
+    plot_height = 400
+  )
+)
+
+
+table(scmapCell_clusters$scmap_cluster_labs)
 
 
