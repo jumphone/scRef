bfbf4cfb0164a8de03354b13a105cc702cd14b1b
Zhang, Feng <jumphone@163.com>
2019-06-02 18:25:28 -0400

    Update COMPARE.sh

--- Benchmark/COMPARE.sh
+++ Benchmark/COMPARE.sh
@@ -1,7 +1,7 @@
 a=read.table('Zeisel_exp_sc_mat.txt',header=T,row.names=1)
-write.table(t(a),'Zeisel_exp_sc_mat.txt.csv',quote=F,sep=',',row.names=T,col.names=T)
+write.table(t(a),'Zeisel_exp_sc_mat.txt.csv',quote=F,sep=',',row.names=T,col.names=F)
 a=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,row.names=1,sep='\t')
-write.table(a,'Zeisel_exp_sc_mat_cluster_original.txt.csv',quote=F,sep=',',row.names=T,col.names=T)
+write.table(a,'Zeisel_exp_sc_mat_cluster_original.txt.csv',quote=F,sep=',',row.names=T,col.names=F)
 
 
 source('Cross_Validation.R')
@@ -17,3 +17,18 @@ LabelsPath='./Zeisel_exp_sc_mat_cluster_original.txt.csv'
 CV_RDataPath='./OUT/CV_folds.RData'
 GeneOrderPath = NULL
 NumGenes = NULL
+
+
+  Data <- read.csv(DataPath,row.names = 1)
+  Labels <- as.matrix(read.csv(LabelsPath))
+  load(CV_RDataPath)
+  Labels <- as.vector(Labels[,col_Index])
+  Data <- Data[Cells_to_Keep,]
+  print(dim(Data))
+  print(dim(Labels))
+
+  Labels <- Labels[Cells_to_Keep]
+  if(!is.null(GeneOrderPath) & !is.null (NumGenes)){
+    GenesOrder = read.csv(GeneOrderPath)
+  }
+
