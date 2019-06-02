411f76d994edeeaa2bb18937377e26b9f9a5295d
Zhang, Feng <jumphone@163.com>
2019-06-02 19:32:00 -0400

    Update COMPARE.sh

--- Benchmark/COMPARE.sh
+++ Benchmark/COMPARE.sh
@@ -3,6 +3,9 @@ write.table(t(a),'Zeisel_exp_sc_mat.txt.csv',quote=F,sep=',',row.names=T,col.nam
 a=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,row.names=1,sep='\t')
 write.table(a,'Zeisel_exp_sc_mat_cluster_original.txt.csv',quote=F,sep=',',row.names=T,col.names=T)
 
+devtools::install_version(package = 'Seurat', version = package_version('2.3.4'))
+
+install.packages("./", repos=NULL, type="source",dep=T)
 
 source('Cross_Validation.R')
 Cross_Validation('./Zeisel_exp_sc_mat_cluster_original.txt.csv',1,'./OUT/')
