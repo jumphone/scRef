76ebb95b33dd9c7a00d1b7ce3564663a43cd36a4
Zhang, Feng <jumphone@163.com>
2019-06-02 18:15:02 -0400

    Update COMPARE.sh

--- Benchmark/COMPARE.sh
+++ Benchmark/COMPARE.sh
@@ -1,1 +1,19 @@
+a=read.table('Zeisel_exp_sc_mat.txt',header=T,row.names=1)
+write.table(t(a),'Zeisel_exp_sc_mat.txt.csv',quote=F,sep=',',row.names=T,col.names=T)
+a=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,row.names=1,sep='\t')
+write.table(a,'Zeisel_exp_sc_mat_cluster_original.txt.csv',quote=F,sep=',',row.names=T,col.names=T)
 
+
+source('Cross_Validation.R')
+Cross_Validation('./Zeisel_exp_sc_mat_cluster_original.txt.csv',1,'./OUT/')
+setwd('../')
+
+source('./scRNAseq_Benchmark-master/Scripts/run_scPred.R')
+run_scPred('Zeisel_exp_sc_mat.txt.csv','./Zeisel_exp_sc_mat_cluster_original.txt.csv','./OUT/CV_folds.RData','~/OUT/')
+
+
+DataPath='Zeisel_exp_sc_mat.txt.csv'
+LabelsPath='./Zeisel_exp_sc_mat_cluster_original.txt.csv'
+CV_RDataPath='./OUT/CV_folds.RData'
+GeneOrderPath = NULL
+NumGenes = NULL
