97b827d6142207cc19ec334d2479128df79c74ab
Zhang, Feng <jumphone@163.com>
2019-06-02 18:33:36 -0400

    Update COMPARE.sh

--- Benchmark/COMPARE.sh
+++ Benchmark/COMPARE.sh
@@ -1,7 +1,7 @@
 a=read.table('Zeisel_exp_sc_mat.txt',header=T,row.names=1)
-write.table(t(a),'Zeisel_exp_sc_mat.txt.csv',quote=F,sep=',',row.names=T,col.names=F)
+write.table(t(a),'Zeisel_exp_sc_mat.txt.csv',quote=F,sep=',',row.names=T,col.names=T)
 a=read.table('Zeisel_exp_sc_mat_cluster_original.txt',header=T,row.names=1,sep='\t')
-write.table(a,'Zeisel_exp_sc_mat_cluster_original.txt.csv',quote=F,sep=',',row.names=T,col.names=F)
+write.table(a,'Zeisel_exp_sc_mat_cluster_original.txt.csv',quote=F,sep=',',row.names=T,col.names=T)
 
 
 source('Cross_Validation.R')
